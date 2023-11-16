e7057ca1fc10c7043aab45a1c2c224afa0ce0e36
NikiforovDmitriy <nikiforov.dmitriy@gmail.com>
2023-11-16 22:41:55 +0600

    add modules

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import geckos, { Data } from '@geckos.io/server'
+// @ts-ignore
+import _ammo from '@enable3d/ammo-on-nodejs/ammo/ammo.js'
+import { Physics, ServerClock } from '@enable3d/ammo-on-nodejs'
