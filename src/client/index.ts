7352216e48532bc9cf414bde91bbddf166c84b84
NikiforovDmitriy <nikiforov.dmitriy@gmail.com>
2023-11-17 18:36:07 +0600

    update client index.ts

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,63 @@
 console.log("I's so cool!")
 
 new EventSource('/esbuild').addEventListener('change', () => location.reload())
+
+import { Project, Scene3D, ExtendedObject3D, THREE } from 'enable3d'
+import geckos from '@geckos.io/client'
+
+class Geckos extends Scene3D {
+    box?: ExtendedObject3D
+    ground?: ExtendedObject3D
+
+    geckos() {
+        const channel = geckos()
+
+        channel.onConnect((error) => {
+            if (error) {
+                console.error(error.message)
+                return
+            }
+
+            channel.on('updates', (updates: any) => {
+                const u = updates
+                if (u.name === 'box') {
+                    if (this.box) {
+                        this.box.position.set(u.pos.x, u.pos.y, u.pos.z)
+                        this.box.quaternion.set(u.quat.x, u.quat.y, u.quat.z, u.quat.w)
+                    } else {
+                        this.box = this.add.box({}, { phong: { color: 'red' } })
+                    }
+                } else if (u.name === 'ground') {
+                    if (!this.ground) {
+                        this.ground = this.add.box(
+                            { width: 40, depth: 40 },
+                            { phong: { color: 'blue' } }
+                        )
+                        this.ground.position.set(u.pos.x, u.pos.y, u.pos.z)
+                        this.ground.quaternion.set(u.quat.x, u.quat.y, u.quat.z, u.quat.w)
+                    }
+                }
+            })
+        })
+    }
+
+    init() {
+        this.setSize(window.innerWidth, window.innerHeight)
+        //this.warpSpeed('-orbitControls')
+        this.camera.position.set(0, 2, 10)
+        this.camera.lookAt(0, 0, 0)
+    }
+
+    create() {
+        this.geckos()
+    }
+
+    update() {
+        if (this.box) {
+            this.camera.position.copy(this.box.position).add(new THREE.Vector3(0, 3, 7))
+            this.camera.lookAt(this.box.position.clone())
+        }
+    }
+}
+
+new Project({ scenes: [Geckos], parent: 'canvas', antialias: true })
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,20 @@
 // @ts-ignore
 import _ammo from '@enable3d/ammo-on-nodejs/ammo/ammo.js'
-import { Physics, ServerClock } from '@enable3d/ammo-on-nodejs'
+import { ExtendedObject3D, Physics, ServerClock } from '@enable3d/ammo-on-nodejs'
 
-import geckos, { Data } from '@geckos.io/server'
+import geckos, { Data, ServerChannel } from '@geckos.io/server'
 import http from 'http'
 import express from 'express'
 
 const port = 3000
 const app = express()
 const server = http.createServer(app)
+const io = geckos({
+    multiplex: true, // default
+})
+
+app.use('/', express.static('public'))
+io.addServer(server)
 
 class ServerScene {
     physics!: Physics
@@ -17,7 +23,15 @@ class ServerScene {
 
     constructor() {
         this.init()
-        //this.create()
+        this.create()
+        this.startServer()
+    }
+
+    startServer() {
+        io.onConnection((channel: ServerChannel) => {})
+        server.listen(port, () => {
+            console.log(`Example app listening on http://localhost:${port}/`)
+        })
     }
 
     init() {
@@ -27,6 +41,60 @@ class ServerScene {
         // init the Physics
         this.physics = new Physics()
         this.factory = this.physics.factory
+    }
+
+    create() {
+        const ground = this.physics.add.box({
+            name: 'ground',
+            width: 40,
+            depth: 40,
+            collisionFlags: 2,
+            mass: 0,
+        })
+
+        this.send(ground)
+
+        const box = this.physics.add.box({ name: 'box', y: 5 })
+
+        this.objects = [ground, box]
+
+        const clock = new ServerClock()
+
+        if (process.env.NODE_ENV !== 'production') clock.disableHighAccuracy()
+
+        clock.onTick((delta) => this.update(delta))
+
+        setInterval(() => this.send(box), 1000 / 30)
+    }
+
+    send(object: ExtendedObject3D) {
+        const { name, position: pos, quaternion: quat } = object
+
+        const fixed = (n: number, f: number) => {
+            return parseFloat(n.toFixed(f))
+        }
+
+        const updates = {
+            name,
+
+            pos: {
+                x: fixed(pos.x, 2),
+                y: fixed(pos.y, 2),
+                z: fixed(pos.z, 2),
+            },
+            quat: {
+                x: fixed(quat.x, 3),
+                y: fixed(quat.y, 3),
+                z: fixed(quat.z, 3),
+                w: fixed(quat.w, 3),
+            },
+        }
+
+        io.emit('updates', updates)
+    }
+
+    update(delta: number) {
+        this.physics.update(delta * 1000)
     }
 }
 
